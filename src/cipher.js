25fddf4bc56c4039b798d2e80b9269318f7dffb2
BarsadeJesus <pattycbs28@gmail.com>
2019-10-11 10:39:03 -0500

    corregi un punto y coma en cipher.js y borre un console log

--- src/cipher.js
+++ src/cipher.js
@@ -1,7 +1,7 @@
 window.cipher = {
   encode: (string, offset) => {
       let cadena='';
-      for(i=0; i<string.length ; i++) {
+      for(let i=0; i<string.length ; i++) {
         //Obtengo el codigo ascii de la letra
         let codeAsci = string.charCodeAt(i);
         if(codeAsci !== 32) {
@@ -15,12 +15,11 @@ window.cipher = {
          }
        }
        return cadena;
-
-
 },
+
 encodeDni: (stringDni, offset) => {
   let cadenaDni='';
-  for(i=0; i<8 ; i++){
+  for(let i=0; i<8 ; i++){
     //Obtengo el codigo ascii del numero
     let codeAsciDni=stringDni.charCodeAt(i);
     //Obtengo la posicion de la nueva posicion(posicion desplazada)
@@ -28,17 +27,16 @@ encodeDni: (stringDni, offset) => {
     //Obtengo la letra de la nueva posicion
     let nDespDni=String.fromCharCode(posDespDni);
     cadenaDni=cadenaDni+nDespDni;
-    };
+    }
     return cadenaDni;
 },
 
 decode: (string,offset) => {
 
      let cadenaD='';
-     for(i=0; i<string.length ; i++){
+     for(let i=0; i<string.length ; i++){
        //Obtengo el codigo ascii de la letra
        let codeAsciD=string.charCodeAt(i);
-       console.log(codeAsciD);
        if (codeAsciD!==32){
        //Obtengo la posicion de la nueva posicion(posicion desplazada)
         let posDespD=((codeAsciD+65 - offset)%26)+65;
@@ -56,7 +54,7 @@ decode: (string,offset) => {
  decodeDni:(stringDni,offset) => {
    let cadenaDniD='';
 
-   for(i=0; i<8 ; i++){
+   for(let i=0; i<8 ; i++){
      //Obtengo el codigo ascii del numero
      let codeAsciDniD=stringDni.charCodeAt(i);
      //Obtengo la posicion de la nueva posicion(posicion desplazada)
